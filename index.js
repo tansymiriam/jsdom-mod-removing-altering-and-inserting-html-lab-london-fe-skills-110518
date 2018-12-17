512371f4730a90554dacbdd32d54665978fc3af5
Tansy Roberts <tansymiriam@gmail.com>
2018-12-17 19:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+var element = document.createElememt('h1')
+
+document.body.appendChild(element)
